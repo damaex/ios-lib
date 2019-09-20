955e19d496899b8e5318bd229524c3b89cb667ab
daMaex <damaex@live.de>
2019-09-20 13:36:05 +0200

    travis debug #5 try opus

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 
 mkdir -p "build"
 
-cd openssl
-bash build.sh
-if [ $? -ne 0 ]; then
-	echo "Error: Build openssl"
-	exit 1
-fi
-cd ..
+#cd openssl
+#bash build.sh
+#if [ $? -ne 0 ]; then
+#	echo "Error: Build openssl"
+#	exit 1
+#fi
+#cd ..
 
-mv openssl/openssl-ios.tar.gz build/
+#mv openssl/openssl-ios.tar.gz build/
 
 cd opus
 bash build.sh
